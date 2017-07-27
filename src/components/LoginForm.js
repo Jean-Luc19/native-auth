a22c311e44af71b7c410aef216c4defc13f79c4b
Aaron Gottlieb <atticus19@gmail.com>
2017-07-27 17:58:47 -0600

    adds Input and Spinner components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common';
+
 
 class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    error: ''
+   };
+
+   onButtonPress() {
+     const { email, password } = this.state;
+     this.setState({ error: '' });
+     firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+   }
   render() {
     return (
       <Card>
         <CardSection>
-          <Button>
+          <Input
+            label="Email"
+            placeholder="friend@youmail.com"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="some password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
@@ -15,5 +54,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
